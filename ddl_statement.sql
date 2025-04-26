956755fa6584f1d184b36faa762d98c1e79a31be
Karthick-LaptopWin <smekarthick2005@gmail.com>
2025-04-27 00:57:39 +0530

    added ddl slq statement

---
+++ ddl_statement.sql
@@ -0,0 +1,361 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema littlelemondb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema littlelemondb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `littlelemondb` DEFAULT CHARACTER SET utf8mb3 ;
+USE `littlelemondb` ;
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`customerdetails`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`customerdetails` (
+  `CustomerID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NOT NULL,
+  `ContactNumber` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`CustomerID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`staffinformation`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`staffinformation` (
+  `StaffID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NOT NULL,
+  `Role` VARCHAR(45) NOT NULL,
+  `Salary` DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (`StaffID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`bookings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`bookings` (
+  `BookingID` INT NOT NULL AUTO_INCREMENT,
+  `CustomerID` INT NOT NULL,
+  `StaffID` INT NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `TableNumber` INT NOT NULL,
+  PRIMARY KEY (`BookingID`),
+  INDEX `customer_fk_idx` (`CustomerID` ASC) VISIBLE,
+  INDEX `staff_fk_idx` (`StaffID` ASC) VISIBLE,
+  CONSTRAINT `customer_fk`
+    FOREIGN KEY (`CustomerID`)
+    REFERENCES `littlelemondb`.`customerdetails` (`CustomerID`),
+  CONSTRAINT `staff_fk`
+    FOREIGN KEY (`StaffID`)
+    REFERENCES `littlelemondb`.`staffinformation` (`StaffID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 17
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`menuitems`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`menuitems` (
+  `MenuItemsID` INT NOT NULL AUTO_INCREMENT,
+  `CourseName` VARCHAR(45) NULL DEFAULT NULL,
+  `StarterName` VARCHAR(45) NULL DEFAULT NULL,
+  `DesertName` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`MenuItemsID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`menus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`menus` (
+  `MenuID` INT NOT NULL AUTO_INCREMENT,
+  `MenuItemsID` INT NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
+  `Type` VARCHAR(45) NOT NULL,
+  `Price` DECIMAL(10,0) NOT NULL,
+  `Cuisine` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`MenuID`),
+  INDEX `menu_items_menu_fk_idx` (`MenuItemsID` ASC) VISIBLE,
+  CONSTRAINT `menus_items_menu_fk`
+    FOREIGN KEY (`MenuItemsID`)
+    REFERENCES `littlelemondb`.`menuitems` (`MenuItemsID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`orders` (
+  `OrderID` INT NOT NULL AUTO_INCREMENT,
+  `CustomerID` INT NOT NULL,
+  `MenuItemID` INT NOT NULL,
+  `StaffID` INT NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `Quantity` INT NOT NULL,
+  `TotalCost` DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (`OrderID`),
+  INDEX `customer_fk_idx` (`CustomerID` ASC) VISIBLE,
+  INDEX `menu_fk_idx` (`MenuItemID` ASC) VISIBLE,
+  INDEX `staff_fk_idx` (`StaffID` ASC) VISIBLE,
+  CONSTRAINT `customer_orders_fk`
+    FOREIGN KEY (`CustomerID`)
+    REFERENCES `littlelemondb`.`customerdetails` (`CustomerID`),
+  CONSTRAINT `menu_orders_fk`
+    FOREIGN KEY (`MenuItemID`)
+    REFERENCES `littlelemondb`.`menus` (`MenuID`),
+  CONSTRAINT `staff_orders_fk`
+    FOREIGN KEY (`StaffID`)
+    REFERENCES `littlelemondb`.`staffinformation` (`StaffID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondb`.`orderdeliverystatuses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`orderdeliverystatuses` (
+  `DeliveryID` INT NOT NULL AUTO_INCREMENT,
+  `OrderID` INT NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `Status` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`DeliveryID`),
+  INDEX `order_fk_idx` (`OrderID` ASC) VISIBLE,
+  CONSTRAINT `order_delivery_fk`
+    FOREIGN KEY (`OrderID`)
+    REFERENCES `littlelemondb`.`orders` (`OrderID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb3;
+
+USE `littlelemondb` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `littlelemondb`.`ordersview`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`ordersview` (`OrderID` INT, `Quantity` INT, `Cost` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `littlelemondb`.`ordersview2`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`ordersview2` (`CustomerID` INT, `FullName` INT, `OrderID` INT, `Cost` INT, `MenuName` INT, `CourseName` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `littlelemondb`.`ordersview3`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`ordersview3` (`MenuName` INT);
+
+-- -----------------------------------------------------
+-- procedure AddBooking
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddBooking`(
+    IN new_booking_id INT, 
+    IN new_customer_id INT, 
+    IN new_booking_date DATE, 
+    IN new_table_number INT, 
+    IN new_staff_id INT)
+BEGIN
+    -- Insert the new booking record
+    INSERT INTO `LittleLemonDB`.`Bookings`(
+        `BookingID`, 
+        `CustomerID`, 
+        `Date`, 
+        `TableNumber`, 
+        `StaffID`)
+    VALUES(
+        new_booking_id, 
+        new_customer_id, 
+        new_booking_date, 
+        new_table_number,
+        new_staff_id
+    );
+
+    SELECT 'New booking added' AS 'Confirmation';
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure AddValidBooking
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddValidBooking`(IN new_booking_date DATE, IN new_table_number INT, IN new_customer_id INT, IN new_staff_id INT)
+BEGIN
+    DECLARE table_status INT;
+    START TRANSACTION;
+    
+    SELECT COUNT(*) INTO table_status
+    FROM `LittleLemonDB`.`Bookings`
+    WHERE `Date` = new_booking_date AND `TableNumber` = new_table_number;
+
+    IF (table_status > 0) THEN
+        ROLLBACK;
+        SELECT 'Booking could not be completed. Table is already booked on the specified date.' AS 'Status';
+    ELSE
+        INSERT INTO `LittleLemonDB`.`Bookings`(`Date`, `TableNumber`, `CustomerID`, `StaffID`)
+        VALUES(new_booking_date, new_table_number, new_customer_id, new_staff_id);
+
+        COMMIT;
+        SELECT 'Booking completed successfully.' AS 'Status';
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure CancelBooking
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CancelBooking`(IN booking_id_to_cancel INT)
+BEGIN
+    -- Delete the booking record
+    DELETE FROM `LittleLemonDB`.`Bookings`
+    WHERE `BookingID` = booking_id_to_cancel;
+
+    SELECT CONCAT('Booking ', booking_id_to_cancel, ' cancelled') AS 'Confirmation';
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure CancelOrder
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CancelOrder`(IN orderIDToDelete INT)
+BEGIN
+  DECLARE orderExistence INT;
+
+  -- Check if the order exists in the database
+  SELECT COUNT(*) INTO orderExistence FROM `LittleLemonDB`.`Orders` WHERE OrderID = orderIDToDelete;
+
+  -- If the order exists, delete it
+  IF orderExistence > 0 THEN
+    -- First delete related records from OrderDeliveryStatuses table
+    DELETE FROM `LittleLemonDB`.`OrderDeliveryStatuses` WHERE OrderID = orderIDToDelete;
+
+    -- Then delete the order from the Orders table
+    DELETE FROM `LittleLemonDB`.`Orders` WHERE OrderID = orderIDToDelete;
+
+    SELECT CONCAT('Order ', orderIDToDelete, ' is cancelled') AS 'Confirmation';
+  ELSE
+    SELECT CONCAT('Order ', orderIDToDelete, ' does not exist') AS 'Confirmation';
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure CheckBooking
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckBooking`(IN booking_date DATE, IN table_number INT)
+BEGIN
+    DECLARE table_status VARCHAR(50);
+
+    SELECT COUNT(*) INTO @table_count
+    FROM `LittleLemonDB`.`Bookings`
+    WHERE `Date` = booking_date AND `TableNumber` = table_number;
+
+    IF (@table_count > 0) THEN
+        SET table_status = 'Table is already booked.';
+    ELSE
+        SET table_status = 'Table is available.';
+    END IF;
+
+    SELECT table_status AS 'Table Status';
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure GetMaxQuantity
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMaxQuantity`()
+BEGIN
+  DECLARE maxQty INT;
+
+  SELECT MAX(Quantity) INTO maxQty FROM `LittleLemonDB`.`Orders`;
+
+  SELECT maxQty AS 'Maximum Ordered Quantity';
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure UpdateBooking
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `littlelemondb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdateBooking`(
+    IN booking_id_to_update INT, 
+    IN new_booking_date DATE)
+BEGIN
+    -- Update the booking record
+    UPDATE `LittleLemonDB`.`Bookings`
+    SET `Date` = new_booking_date
+    WHERE `BookingID` = booking_id_to_update;
+
+    SELECT CONCAT('Booking ', booking_id_to_update, ' updated') AS 'Confirmation';
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- View `littlelemondb`.`ordersview`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `littlelemondb`.`ordersview`;
+USE `littlelemondb`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `littlelemondb`.`ordersview` AS select `littlelemondb`.`orders`.`OrderID` AS `OrderID`,`littlelemondb`.`orders`.`Quantity` AS `Quantity`,`littlelemondb`.`orders`.`TotalCost` AS `Cost` from `littlelemondb`.`orders` where (`littlelemondb`.`orders`.`Quantity` > 2);
+
+-- -----------------------------------------------------
+-- View `littlelemondb`.`ordersview2`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `littlelemondb`.`ordersview2`;
+USE `littlelemondb`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `littlelemondb`.`ordersview2` AS select `cd`.`CustomerID` AS `CustomerID`,`cd`.`Name` AS `FullName`,`o`.`OrderID` AS `OrderID`,`o`.`TotalCost` AS `Cost`,`m`.`Name` AS `MenuName`,`mi`.`CourseName` AS `CourseName` from (((`littlelemondb`.`customerdetails` `cd` join `littlelemondb`.`orders` `o` on((`o`.`CustomerID` = `cd`.`CustomerID`))) join `littlelemondb`.`menus` `m` on((`o`.`MenuItemID` = `m`.`MenuItemsID`))) join `littlelemondb`.`menuitems` `mi` on((`m`.`MenuItemsID` = `mi`.`MenuItemsID`))) order by `o`.`TotalCost`;
+
+-- -----------------------------------------------------
+-- View `littlelemondb`.`ordersview3`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `littlelemondb`.`ordersview3`;
+USE `littlelemondb`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `littlelemondb`.`ordersview3` AS select `m`.`Name` AS `MenuName` from `littlelemondb`.`menus` `m` where `m`.`MenuItemsID` in (select `o`.`MenuItemID` from `littlelemondb`.`orders` `o` group by `o`.`MenuItemID` having (sum(`o`.`Quantity`) > 2));
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
